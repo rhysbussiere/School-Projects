08c8490763ede54a82b6124f3214a2396de1078d
Rhys Bussiere <34968408+MisterMeeseek5@users.noreply.github.com>
2018-01-27 18:16:10 -0800

    adding projects

---
+++ CS-ASP_008/CS-ASP_008/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void goButton_Click(object sender, EventArgs e)
+        {
+            /*
+            int i;
+            i = "3";
+            */
+
+            /*
+            int i = 200000000;
+            long j = i;
+            */
+
+            /* -- DOWN CAST --
+            long i = 200000000;
+            int j = (int)i;
+            -- CAN CAUSE DATA LOSS -- */
+
+            /*
+            double k = 2.5;
+            int j = (int)k;
+
+            //resultLabel.Text = j;
+            //resultLabel.Text = (string)j;
+            resultLabel.Text = j.ToString();
+            */
+
+            string i = inputTextBox.Text;
+            int j = int.Parse(i);
+            int k = j + 1;
+            resultLabel.Text = k.ToString();
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(firstTextBox.Text);
+            double secondValue = Convert.ToDouble(secondTextBox.Text);
+
+            double result = firstValue + secondValue;
+
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(firstTextBox.Text);
+            double secondValue = Convert.ToDouble(secondTextBox.Text);
+
+            double result = firstValue - secondValue;
+
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(firstTextBox.Text);
+            double secondValue = Convert.ToDouble(secondTextBox.Text);
+
+            double result = firstValue * secondValue;
+
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(firstTextBox.Text);
+            double secondValue = Convert.ToDouble(secondTextBox.Text);
+
+            double result = firstValue / secondValue;
+
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
